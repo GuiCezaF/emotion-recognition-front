d444d378860eb4a41dfc93e22597d36142543646
gabriell3103 <g5819853@gmail.com>
2025-10-29 19:24:42 -0300

    feat: páginas sem conteúdo

--- src/pages/Acessibility.tsx
+++ src/pages/Acessibility.tsx
@@ -1,5 +1,11 @@
+import { FaRegSadTear } from 'react-icons/fa';
+
 export const Acessibility = () => {
   return (
-    <div>Acessibility</div>
-  )
+    <div className="flex flex-col items-center justify-center align-middle h-full w-full bg-white">
+      <span className="flex items-center gap-2 text-xl mt-5">
+         Não temos nada nessa tela ainda <FaRegSadTear />
+      </span>
+    </div>
+      );
 }
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,6 +1,11 @@
+import { FaRegSadTear } from 'react-icons/fa';
 
 export const Profile = () => {
   return (
-    <div>Profile</div>
-  )
+    <div className="flex flex-col items-center justify-center align-middle h-full w-full bg-white">
+      <span className="flex items-center gap-2 text-xl mt-5">
+         Não temos nada nessa tela ainda <FaRegSadTear />
+      </span>
+    </div>
+      );
 }
